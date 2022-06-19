eae96e7be101d6e141ef17297f0082ad0d4ed2a7
abhimhamane <abhi.mhamane30@gmail.com>
2022-06-19 22:28:46 +0530

    some changes made to continuous beam analysis

--- conti_beam_analysis.py
+++ conti_beam_analysis.py
@@ -12,4 +12,12 @@ from PIL import Image
 
 def equally_spaced_sprts(cont_beam_inst, num_spans):
     _num_sprts = num_spans + 1
-    return linspace(0, cont_beam_inst.legth, _num_sprts)
+    return linspace(0, cont_beam_inst.length, _num_sprts)
+
+def span_list(num_spans):
+    _opt_list = []
+    for _span_no in range(num_spans):
+        _opt_list.append("span "+str(_span_no + 1))
+    
+    return _opt_list
+
--- pages/Continious_Beam_Analysis.py
+++ pages/Continious_Beam_Analysis.py
@@ -13,15 +13,21 @@ from conti_beam_analysis import *
 cont_beam_analysis_params = st.sidebar.form("Continuous_beam_analysis_parameters")
 cont_beam_analysis_params.header("Continuous Beam Analysis Parameters")
 total_length = cont_beam_analysis_params.slider("Length (m)", 5.0, 35.0, 10.0, step=1.0)
-num_spans = cont_beam_analysis_params.slider("Number of spans:", 1.0, 4.0, 3.0, step=1.0)
+num_spans = cont_beam_analysis_params.slider("Number of spans:", 2.0, 4.0, 3.0, step=1.0)
 type_of_spans = cont_beam_analysis_params.radio("Nature of Spans:", options=['Equal', 'Unequal'], horizontal=True)
 
 BM_preference = cont_beam_analysis_params.radio("Choose your BM Drawing convention:", options=['Compression Side' ,'Tension Side'], horizontal=True)
 cont_beam_analysis_params.form_submit_button("Apply Changes")
 
 #------------Main Content --------------#
-cont_header = st.container()
-cont_header.title("Continuous Beam Analysis")
+
+st.title("Continuous Beam Analysis")
+
+if num_spans <= 2:
+    sprt_cond = st.radio("Choose End Support Conditions:", options=['Fix-Fix', 'Fix-Pin', 'Fix-Free', 'Pin-Pin', 'Pin-Free'], horizontal=True)
+elif num_spans > 2:
+    sprt_cond = st.radio("Choose End Support Conditions:", options=['Fix-Fix', 'Fix-Pin', 'Fix-Free', 'Pin-Pin', 'Pin-Free', 'Free-free'], horizontal=True)
+
 
 if type_of_spans == "Equal":
     equal_spans_end_sprts = st.container()
@@ -30,8 +36,10 @@ if type_of_spans == "Equal":
     num_supports = num_spans + 1
 
     if 'num_supports' not in st.session_state:
-            equal_spans_end_sprts.session_state.num_supports = num_supports
+            st.session_state.num_supports = num_supports
 
+elif type_of_spans == "Unequal":
+    
     
 
 
